10fee5201f88d36acd32e4182fad32dcb7c64ee4
dirt-boy <gabriel.v.gagnon@gmail.com>
2021-04-06 15:50:33 -0700

    added basic auth functionality

--- stripe_read.py
+++ stripe_read.py
@@ -1,5 +1,25 @@
 import stripe
+import os
+import pickle
 
-stripe.api_key = "SECRET"
-balance_transactions = stripe.BalanceTransaction.list(limit=3)
-print(balance_transactions)
+class Error(Exception):
+	#Base Class
+	pass
+
+class AuthError(Error):
+
+	message = "Unable to complete authorization: Missing or Incorrect authorization token."
+
+	def __init__(self, message):
+		self.message = message
+
+
+if os.path.exists('data/token.pickle'):
+	try:
+		stripe.api_key = pickle.load(open('data/token.pickle', 'rb'))
+		balance_transactions = stripe.BalanceTransaction.list(limit=3)
+		print(balance_transactions)
+	except AuthError as e:
+		print(e.message)
+else:
+	print("Unable to complete authorization: unknown error.")
