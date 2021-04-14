2d7d3f87b764505bf95e2f71cf6498c1b403a05f
dirt-boy <gabriel.v.gagnon@gmail.com>
2021-04-13 18:12:30 -0700

    ++csv writing, global vars, TransactionRecord class

--- stripe_read.py
+++ stripe_read.py
@@ -1,25 +1,104 @@
 import stripe
 import os
+import csv
 import pickle
+from datetime import date
 
-class Error(Exception):
-	#Base Class
-	pass
+### GLOBAL VARIABLES ##############################################################################
+#																								  #	
+DATE = date.today().strftime('%m_%d_%y')													  #
+#																								  #
+### END GLOBAL VARIABLES ##########################################################################
 
-class AuthError(Error):
 
-	message = "Unable to complete authorization: Missing or Incorrect authorization token."
-
+### CUSTOM ERROR CLASSES ##########################################################################
+#																								  #
+class Error(Exception):													  					  	  #
+	#Base class  																			      #
+	pass 																					      #
+#																								  #
+class AuthError(Error):																		      #
+#																								  #
+	message = "Unable to complete authorization: Missing or Incorrect authorization token."       #																						  #
+	def __init__(self, message):															      #
+		self.message = message 																  #
+#	
+class FileError(Error):
+	message = "Unable to open file. File may be corrupted."
 	def __init__(self, message):
 		self.message = message
 
+class WriteError(Error):
+	message = "Unable to write file. File or data may be incompatible and/or corrupted."
+	def __init(self, message):
+		self.message = message
+#																							      #
+### END CUSTOM ERROR CLASSES ######################################################################
 
-if os.path.exists('data/token.pickle'):
+### CUSTOM DATA STRUCTURE CLASSES #################################################################
+#																								  #
+class TransactionRecord:
+	def __init__(self, balance_transactions, charges):
+		self.balance_transactions = balance_transactions
+		self.charges = charges
+#																							      #
+### END CUSTOM DATA STRUCTURE CLASSES #############################################################
+
+
+#build func to ask for auth key
+
+def read():
+	if os.path.exists('data/token.pickle'):
+		try:
+			#auth with saved key
+			stripe.api_key = pickle.load(open('data/token.pickle', 'rb'))
+
+			#pull txn_ and ch_ where limit is clamp on # of transactions returned
+			balance_transactions = stripe.BalanceTransaction.list(limit=3)
+			charges = stripe.Charge.list(limit=3)
+
+			data = TransactionRecord(balance_transactions, charges)
+
+			#return TransactionRecord
+			return(data)
+
+		#catch AuthError	
+		except AuthError as e:
+			print(e.message)
+	else:
+		print("Unable to complete authorization: token not found.")
+
+
+def write(charges):
+	#Prep vars
+	headers = charges['data'][0].keys()
+	values = []
+	for i, val in enumerate(charges['data']):
+		values.append(charges['data'][i].values())
+	path = 'results/balance_transactions_%s.csv' % DATE
+
+	
 	try:
-		stripe.api_key = pickle.load(open('data/token.pickle', 'rb'))
-		balance_transactions = stripe.BalanceTransaction.list(limit=3)
-		print(balance_transactions)
-	except AuthError as e:
+		file = open(path, 'w+')
+		writer = csv.writer(file)
+		try:
+			writer.writerow(headers)
+			for v in values:
+				writer.writerow(v)
+		except WriteError as e:
+			print(e.message) 
+	except FileError as e:
 		print(e.message)
-else:
-	print("Unable to complete authorization: unknown error.")
+		
+
+
+
+
+
+
+
+data = read()
+write(data.charges)
+
+
+
